2b7663a53cabc141be8cde2c4693478f9827cd33
ShunSakakida <shun.sakakida@kenedix-st.com>
2024-10-26 20:33:20 +0900

    dynamo作成

--- modules/state_s3/main.tf
+++ modules/state_s3/main.tf
@@ -1,9 +1,29 @@
-# modules/s3/main.tf
+locals {
+  resource_name = "${var.identifier}-for-state"
+}
+
+
+# Stateファイルを保存するS3
 resource "aws_s3_bucket" "state_bucket" {
-  bucket = "${var.identifier}-for-state"  # プレフィックスを使用
-  acl    = "private"                      # アクセス制御リスト（ACL）
+  bucket = local.resource_name
+  acl    = "private" # アクセス制御リスト（ACL）
 
   tags = {
-    Name        = "${var.identifier}"
+    Name = "${var.identifier}"
+  }
+}
+
+# Stateファイルのロックを管理するDynamoDB
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = local.resource_name
+  billing_mode = "PAY_PER_REQUEST"
+   attribute {
+     name = "LockID"
+     type = "S"
+   }
+   hash_key = "LockID"
+
+  tags = {
+    Name = "${var.identifier}"
   }
 }
