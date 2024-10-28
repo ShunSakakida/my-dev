2d88fb44e976b92250d870f3d09bd0ff081483a0
ShunSakakida <shun.sakakida@kenedix-st.com>
2024-10-28 23:09:58 +0900

    main変更

--- src/lambda/main.go
+++ src/lambda/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,12 +12,31 @@ type MyEvent struct {
 	Name string `json:"name"`
 }
 
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+type Response struct {
+	IsBase64Encoded bool              `json:"isBase64Encoded"`
+	StatusCode      int               `json:"statusCode"`
+	Headers         map[string]string `json:"headers"`
+	Body            string            `json:"body"`
+}
+
+func HandleRequest(ctx context.Context, event *MyEvent) (*Response, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
 	message := fmt.Sprintf("Hello %s!", event.Name)
-	return &message, nil
+	body, err := json.Marshal(map[string]string{"message": message})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Response{
+		IsBase64Encoded: false,
+		StatusCode:      200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(body),
+	}, nil
 }
 
 func main() {
